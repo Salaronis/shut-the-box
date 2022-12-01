2f9560d33ac288030f6215a45489982f00a6e7a3
Michael Xie <mxie0114@glamil.com>
2022-12-01 16:07:30 -0500

    Adding Event Listener and Other Support Buttons

--- Main.java
+++ Main.java
@@ -1,18 +1,10 @@
-import javax.swing.*;  
-import java.lang.Math;
+
 
 public class Main{
     public static void main(String[] args) {
-        boolean small = false;
         Setup.create();
     }
-    static int roll(boolean small){
-        if(small){
-            return 1 + (int)(Math.floor(Math.random()*6));
-        }
-        return 1 + (int)(Math.floor(Math.random()*12));
-    }
-    static void check(){
+    
 
-    }
 }
+
--- Setup.java
+++ Setup.java
@@ -1,18 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashSet;
+
 import javax.swing.*;  
-public class Setup {
-    public static void create(){
-        JFrame frame = new JFrame();
-        JButton[] array = new JButton[10];
-        JButton oneButton = new JButton("1");//creating instance of JButton  
-        JButton twoButton = new JButton("2");
-        JButton threeButton = new JButton("3");
-        JButton fourButton = new JButton("3");
-        JButton fiveButton = new JButton("5");
-        JButton sixButton = new JButton("6");
-        JButton sevenButton = new JButton("7");
-        JButton eightButton = new JButton("8");
-        JButton nineButton = new JButton("9");
-        JButton roll = new JButton("Roll");
+public class Setup{
+    static JFrame frame = new JFrame("Shut the Box");
+    static boolean small = false;
+    static JButton[] array = new JButton[9];
+    //Number Buttons
+    static JButton oneButton = new JButton("1");//creating instance of JButton  
+    static JButton twoButton = new JButton("2");
+    static JButton threeButton = new JButton("3");
+    static JButton fourButton = new JButton("3");
+    static JButton fiveButton = new JButton("5");
+    static JButton sixButton = new JButton("6");
+    static JButton sevenButton = new JButton("7");
+    static JButton eightButton = new JButton("8");
+    static JButton nineButton = new JButton("9");
+    //Auxillary buttons
+    static JButton roll = new JButton("Roll");
+    static JButton clear = new JButton("Clear");
+    static JButton confirm = new JButton("Confirm");
+    
+    static int rollVal;
+    static int selectVal = 0;
+    static HashSet<Integer> set = new HashSet<>();
+    public static void create() {
         array[0] = oneButton;
         array[1] = twoButton;
         array[2] = threeButton;
@@ -22,14 +35,40 @@ public class Setup {
         array[6] = sevenButton;
         array[7] = eightButton;
         array[8] = nineButton;
-        array[9] = roll;
         for(int i = 0; i < 9; i++){
-            frame.add(array[i]);
-            array[i].setBounds(i*110,0,100,200);
+            JButton sel = array[i];
+            frame.add(sel);
+            sel.addActionListener( new ActionListener(){
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(sel.isEnabled()){
+                        selectVal += Integer.parseInt(sel.getText());
+                    }else{
+                        selectVal -= Integer.parseInt(sel.getText());
+                    }
+                    sel.setEnabled(!sel.isEnabled());
+                }  
+            });
+            array[i].setBounds((i*110)+2,0,100,200);
         }
+        frame.add(roll);
+        roll.setBounds(350,450,300,100);
         frame.setSize(1000,600);//400 width and 500 height  
         frame.setLayout(null);//using no layout managers  
         frame.setVisible(true);//making the framerame visible  
     }
+    public static boolean check(){
+        return rollVal == selectVal;
+    }
+    static int roll(boolean small){
+        int val;
+        if(small){
+            val = 1 + (int)(Math.floor(Math.random()*6));
+        }else{
+            val = 1 + (int)(Math.floor(Math.random()*12));
+        }
+        return val;
+    }
+
     
 }
